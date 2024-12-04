ba9e7ed0e658d7a3ee8104e740c7f79f5104969c
kayligregory <90810167+kayligregory@users.noreply.github.com>
2024-12-03 22:30:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,32 +11,39 @@ import './App.css';
 import firebaseConfig from './firebase/FirebaseConfig.js';
 import { AuthProvider } from './components/AuthContext.js';
 import PrivateRoute from './components/PrivateRoute.js';
-import {initializeApp} from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <AuthProvider>
       <Navigation />
-        <Routes>
-          <Route path="/" element={<Navigate to="/signin" replace />} />
-          <Route path='/home' element={<PrivateRoute />}>
-            <Route path='/home' element={<HomePage />} />
-          </Route>
-          <Route path='/account' element={<PrivateRoute />}>
-            <Route path='/account' element={<Account />} />
-          </Route>
-          <Route path='/find' element={<PrivateRoute />}>
-            <Route path='/find' element={<FindPage />} />
-          </Route>
-          <Route path='/about' element={<PrivateRoute />}>
-            <Route path='/about' element={<AboutPage />} />
-          </Route>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
+      <Routes>
+        {/* Redirect "/" to "/home" */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
+
+        {/* Protected Routes */}
+        <Route path="/home" element={<PrivateRoute />}>
+          <Route path="/home" element={<HomePage />} />
+        </Route>
+        <Route path="/account" element={<PrivateRoute />}>
+          <Route path="/account" element={<Account />} />
+        </Route>
+        <Route path="/find" element={<PrivateRoute />}>
+          <Route path="/find" element={<FindPage />} />
+        </Route>
+        <Route path="/about" element={<PrivateRoute />}>
+          <Route path="/about" element={<AboutPage />} />
+        </Route>
+
+        {/* Public Routes */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
